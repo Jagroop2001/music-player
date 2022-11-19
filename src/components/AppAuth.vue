dc785460e148171981ea83710035412ac2c7a85b
Jagroop Singh <jagroopofficial2000@gmail.com>
2022-11-19 12:57:23 +0530

    Validation Part Done

--- src/components/AppAuth.vue
+++ src/components/AppAuth.vue
@@ -41,84 +41,13 @@
                     </ul>
 
                     <!-- Login Form -->
-                    <form v-show="tab === 'login'">
-                        <!-- Email -->
-                        <div class="mb-3">
-                            <label class="inline-block mb-2">Email</label>
-                            <input type="email"
-                                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                                placeholder="Enter Email" />
-                        </div>
-                        <!-- Password -->
-                        <div class="mb-3">
-                            <label class="inline-block mb-2">Password</label>
-                            <input type="password"
-                                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                                placeholder="Password" />
-                        </div>
-                        <button type="submit"
-                            class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700">
-                            Submit
-                        </button>
-                    </form>
+                    <app-login-form v-if="tab === 'login'"></app-login-form>
+
                     <!-- Registration Form -->
-                    <vee-form v-show="tab === 'register'" :validation-schema="schema">
-                        <!-- Name -->
-                        <div class="mb-3">
-                            <label class="inline-block mb-2">Name</label>
-                            <vee-field type="text" name="name"
-                                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                                placeholder="Enter Name" />
-                            <ErrorMessage class="text-red-600" name="name" />
-                        </div>
-                        <!-- Email -->
-                        <div class="mb-3">
-                            <label class="inline-block mb-2">Email</label>
-                            <vee-field type="email" name="email"
-                                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                                placeholder="Enter Email" />
-                            <ErrorMessage class="text-red-600" name="email" />
-                        </div>
-                        <!-- Age -->
-                        <div class="mb-3">
-                            <label class="inline-block mb-2">Age</label>
-                            <input type="number"
-                                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded" />
-                        </div>
-                        <!-- Password -->
-                        <div class="mb-3">
-                            <label class="inline-block mb-2">Password</label>
-                            <input type="password"
-                                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                                placeholder="Password" />
-                        </div>
-                        <!-- Confirm Password -->
-                        <div class="mb-3">
-                            <label class="inline-block mb-2">Confirm Password</label>
-                            <input type="password"
-                                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                                placeholder="Confirm Password" />
-                        </div>
-                        <!-- Country -->
-                        <div class="mb-3">
-                            <label class="inline-block mb-2">Country</label>
-                            <select
-                                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded">
-                                <option value="USA">USA</option>
-                                <option value="Mexico">Mexico</option>
-                                <option value="Germany">Germany</option>
-                            </select>
-                        </div>
-                        <!-- TOS -->
-                        <div class="mb-3 pl-6">
-                            <input type="checkbox" class="w-4 h-4 float-left -ml-6 mt-1 rounded" />
-                            <label class="inline-block">Accept terms of service</label>
-                        </div>
-                        <button type="submit"
-                            class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700">
-                            Submit
-                        </button>
-                    </vee-form>
+                    <app-register-from v-else></app-register-from>
+                    <!-- <register-form v-else /> -->
+
+
                 </div>
             </div>
         </div>
@@ -127,22 +56,18 @@
 
 <script>
 import { mapState, mapWritableState } from 'pinia';
-import { ErrorMessage } from 'vee-validate';
 import useModalStore from '../stores/modal.js'
+import AppLoginForm from './LoginForm.vue';
+import AppRegisterFrom from './RegisterFrom.vue';
 export default {
     name: "AppAuth",
+    components: {
+        AppLoginForm,
+        AppRegisterFrom
+    },
     data() {
         return {
             tab: "login",
-            schema: {
-                name: "required|min:3|max:100|alpha_spaces",
-                email: "required|min:3|max:100|email",
-                age: "",
-                password: "",
-                confirm_password: "",
-                country: "",
-                tos: ""
-            }
         };
     },
     computed: {
@@ -151,7 +76,7 @@ export default {
             modalVisibility: "isOpen",
         })
     },
-    components: { ErrorMessage }
+
 }
 
 </script>
--- src/includes/validation.js
+++ src/includes/validation.js
@@ -1,5 +1,8 @@
-import { required, min , max , alpha_spaces as alphaSpaces , email } from "@vee-validate/rules";
-import { Form as VeeForm , Field as VeeField , defineRule , ErrorMessage} from "vee-validate"
+import { required, min , max , alpha_spaces as alphaSpaces , email ,
+    min_value as minValue , max_value as maxValue , confirmed , not_one_of as excluded
+} from "@vee-validate/rules";
+import { Form as VeeForm , Field as VeeField , defineRule , ErrorMessage, configure
+} from "vee-validate"
 
 
 
@@ -10,9 +13,37 @@ export default {
          app.component('ErrorMessage', ErrorMessage)
 
          defineRule('required',required)
+         defineRule('tos',required)
          defineRule('min' , min)
          defineRule('max' , max)
          defineRule('alpha_spaces',alphaSpaces)
          defineRule('email', email)
-    }
+         defineRule('min_value',minValue)
+         defineRule('max_value',maxValue)
+         defineRule('confirmed',confirmed)
+         defineRule('excluded',excluded)
+
+    configure({
+        generateMessage:(ctx) => {
+            const messages = {
+                required : `The field ${ctx.field}  is required`,
+                min : `The field ${ctx.field}  is too short`,
+                max : `The field ${ctx.field}  is too long`,
+                alpha_spaces : `The field ${ctx.field}  may only contain alphabetic characters and spaces`,
+                email : `The field ${ctx.field}  must be a valid email`,
+                min_value : `The field ${ctx.field}  is too low`,
+                max_value : `The field ${ctx.field}  is too high`,
+                excluded : `You are not allowed to use this value for the ${ctx.field}`,
+                confirm : `Password don't match.`,
+                tos:'You must accept the Terms of Service.'
+            }
+
+            const message =  messages[ctx.rule.name] ? messages[ctx.rule.name] : `The field ${ctx.field}  is not valid`;
+            return message;
+        }
+    })
+},
+validateOnBlur : true,
+validateOnChange : true,
+validateOnInput : false
 }
